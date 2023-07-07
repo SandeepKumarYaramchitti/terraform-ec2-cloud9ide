44da5e7d68a9cacc9ae740e28e22f0ecd64eb8b8
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-07-07 08:22:57 -0400

    Revert "add public access"
    This reverts commit e352f0bb7dc5f82dc24bada3e2c10fe9e3e94666.

--- main.tf
+++ main.tf
@@ -32,25 +32,6 @@ data "aws_subnet" "existing_subnet" {
   }
 }
 
-resource "aws_security_group" "cloud9_sg" {
-  name        = "cloud9-sg"
-  description = "Security group for Cloud9 environment"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Set uo Cloud9 IDE
 resource "aws_cloud9_environment_ec2" "cloud9_instance" {
   name                        = "cloud9_instance"
@@ -58,32 +39,29 @@ resource "aws_cloud9_environment_ec2" "cloud9_instance" {
   automatic_stop_time_minutes = 30
   subnet_id                   = data.aws_subnet.existing_subnet.id
 
-  security_group_ids = [
-    aws_security_group.cloud9_sg.id
-  ]
   tags = {
     Terraform = "true"
   }
 }
 
-# data "aws_security_group" "cloud9_secgroup" {
-#   filter {
-#     name = "tag:aws:cloud9:environment"
-#     values = [
-#       aws_cloud9_environment_ec2.cloud9_instance.id
-#     ]
-#   }
-# }
-# # Allow public access to port 8080
-# resource "aws_security_group_rule" "tcp_8080" {
-#   type              = "ingress"
-#   from_port         = 8080
-#   to_port           = 8080
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"] 
-#   ipv6_cidr_blocks  = ["::/0"]
-#   security_group_id = data.aws_security_group.cloud9_secgroup.id
-# }
+data "aws_security_group" "cloud9_secgroup" {
+  filter {
+    name = "tag:aws:cloud9:environment"
+    values = [
+      aws_cloud9_environment_ec2.cloud9_instance.id
+    ]
+  }
+}
+# Allow public access to port 8080
+resource "aws_security_group_rule" "tcp_8080" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = data.aws_security_group.cloud9_secgroup.id
+}
 
 
 output "cloud9_url" {
